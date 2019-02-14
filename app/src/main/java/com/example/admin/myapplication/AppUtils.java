51821aca6ae1492ea91f61dc0ba4eec93809064c
hfzj <hfzj1311@163.com>
2019-02-14 14:25:55 +0800

    第二次添加

---
+++ app/src/main/java/com/example/admin/myapplication/AppUtils.java
@@ -0,0 +1,20 @@
+package com.example.admin.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin on 2019/2/14.
+ */
+
+public class AppUtils  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
